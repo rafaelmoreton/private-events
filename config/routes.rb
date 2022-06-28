2a0491fbd18c34e6bea4245f126a4c5753e9f419
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-28 15:42:44 -0300

    List events created by the user on it's user/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'events/index'
+  get 'events/index', to: 'events#index'
+  get 'users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
