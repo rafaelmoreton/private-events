2551ff9f3208780ae5cccd3a56eaac16e6b4c979
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-30 12:40:50 -0300

    Refactor past and future events query

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations
   belongs_to :creator, class_name: "User"
+
+  scope :past, -> { where("date < ?", DateTime.now) }
+  scope :future, -> { where("date > ?", DateTime.now) }
 end
