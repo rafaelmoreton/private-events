867f65aa6fd4cc8bf3514eae9fdd551d675a7426
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-30 13:01:42 -0300

    Validate all the fields for new events forms
    Exchange client side only validation for server side for new events' date

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invitations
   belongs_to :creator, class_name: "User"
 
+  validates :title, :location, :description, presence: true
+  validates :date , comparison: { greater_than: Date.today }
+
   scope :past, -> { where("date < ?", DateTime.now) }
   scope :future, -> { where("date > ?", DateTime.now) }
 end
