e55f173b3ed920154e4b3f0e7fa674dc41ddb054
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-30 11:31:49 -0300

    Require user authentication before certain actions

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,7 @@
 class InvitationsController < ApplicationController
+  before_action :store_location
+  before_action :authenticate_user!, only: %i[ create ]
+
   def create
     @invitation = Invitation.new
     @invitation.attendee_id = current_user.id
@@ -9,5 +12,11 @@ class InvitationsController < ApplicationController
     else
        redirect_to event_path(params[:invitation][:event_id]), alert: "Couldn't attend the event"
     end
+  end
+
+  private
+
+  def store_location
+    session[:user_return_to] = event_url(params[:invitation][:event_id])
   end
 end
