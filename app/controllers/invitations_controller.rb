e748c61c7957661a70e358d600a787fddcf27967
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-07-02 10:09:25 -0300

    Only allow to cancel attendance on attended events

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,6 @@
 class InvitationsController < ApplicationController
   before_action :store_location
-  before_action :authenticate_user!, only: %i[ create ]
+  before_action :authenticate_user!, only: %i[ create destroy ]
 
   def create
     @invitation = Invitation.new
@@ -16,7 +16,7 @@ class InvitationsController < ApplicationController
 
   def destroy
     @invitation = Invitation.where("attendee_id = ? AND attended_event_id = ?", current_user.id, params[:event_id]).take
-    @invitation.destroy
+    @invitation&.destroy
 
     redirect_to event_path
   end
